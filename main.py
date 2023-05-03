b5973a8f410aca9914ec8cc6296cb9310c3a7057
Rafay Qayyum <rafayqayyum786@gmail.com>
2023-05-03 21:54:01 +0500

    fixed empty schema bug

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with st.sidebar:
     st.write("I am a software engineer and a ML enthusiast. I am passionate about building products and solving problems.")
     st.write("Find me on [Github](https://github.com/rafayqayyum) and [LinkedIn](https://www.linkedin.com/in/rafayqayyum/)")
     st.write("This is a simple text summarizer app built using [OpenAI](https://openai.com/) and [Streamlit](https://streamlit.io/).")
-    st.write("You can find the source code [here](https://github.com/Rafayqayyum/Text-Summarizer)")
+    st.write("You can find the source code [here](https://github.com/Rafayqayyum/text-to-sql)")
 
 with st.container():
     st.title("SQL Generator")
@@ -40,20 +40,23 @@ with st.container():
     if st.button("Generate SQL"):
         if db_type!=None and file!=None and file!="" and text!="":
             filename=convert_to_file(file)
-            schema=read_file(filename)
-            # generate sql highlight code and sql keywords
-            status,sql,token_discarded= generate_sql("Schema: "+schema+"\n"+text,db_type)
-            if status:
-                # display sql query and highlight code and sql keywords
-                st.subheader("SQL Query:")
-                # show sql query and format it as Select, From, Where, Group By, Order By etc on new lines
-                sql_formatted=sql.replace('SELECT','\nSELECT').replace('FROM','\nFROM').replace('WHERE','\nWHERE').replace('GROUP BY','\nGROUP BY').replace('ORDER BY','\nORDER BY').replace('LIMIT','\nLIMIT').replace('HAVING','\nHAVING')
-
-                st.code(sql_formatted,language='sql')
-            elif sql==None:
-                st.error("Error generating SQL query.")
+            schema=read_file(filename).strip()
+            if schema==None or schema=="":
+                st.error("Schema file is empty.")
             else:
-                st.error(sql)
+                # generate sql highlight code and sql keywords
+                status,sql,token_discarded= generate_sql(schema,text,db_type)
+                if status:
+                    # display sql query and highlight code and sql keywords
+                    st.subheader("SQL Query:")
+                    # show sql query and format it as Select, From, Where, Group By, Order By etc on new lines
+                    sql_formatted=sql.replace('SELECT','\nSELECT').replace('FROM','\nFROM').replace('WHERE','\nWHERE').replace('GROUP BY','\nGROUP BY').replace('ORDER BY','\nORDER BY').replace('LIMIT','\nLIMIT').replace('HAVING','\nHAVING')
+
+                    st.code(sql_formatted,language='sql')
+                elif sql==None:
+                    st.error("Error generating SQL query.")
+                else:
+                    st.error(sql)
         elif db_type==None:
             st.error("Please provide Database type.")
         elif file==None or file=="":
